e0cfe7476a4a5fee544fff214dcbe94f217b29ab
SandyKnluck <nimnaul_k@su.ac.th>
2024-12-21 16:00:32 +0700

    Create Container.

--- demoproject/lib/main.dart
+++ demoproject/lib/main.dart
@@ -19,24 +19,30 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(
-            title: const Text("Flutter Demo",
-                style: TextStyle(color: Color.fromARGB(255, 183, 63, 3))),
+            title: const Text("Flutter Demo \u{1F984}",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 152, 113, 171),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold)),
             leading: const Icon(Icons.add),
-            backgroundColor: Color.fromARGB(255, 255, 192, 160),
+            backgroundColor: Color.fromARGB(255, 255, 211, 231),
           ),
           body: Container(
-            height: 100,
-            width: 100,
-            color: Color.fromARGB(255, 255, 242, 160),
+            color: Color.fromARGB(255, 254, 197, 255),
             padding: EdgeInsets.all(20),
             margin: const EdgeInsets.all(20),
             child: const Text('Hello'),
           ),
           floatingActionButton: FloatingActionButton(
+            backgroundColor: Color.fromARGB(255, 255, 205, 233),
             onPressed: () {
               print("Hello");
             },
-            child: const Icon(Icons.phone),
+            child: const Icon(
+              Icons.phone,
+              color: Color.fromARGB(255, 72, 162, 189),
+              size: 37,
+            ),
           ),
         )
         //const Text('Hello'),
